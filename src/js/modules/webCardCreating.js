db931104583f60aed9ac1cb55d3eb9f2d79f9c2f
SLain <sl163@ya.ru>
2020-08-26 12:51:31 +0300

    Добавляет изменения в скрипт генерации карточек, карточки формируются на основе информации поступившей из БД db.json, запрос через fetch;

--- src/js/modules/webCardCreating.js
+++ src/js/modules/webCardCreating.js
@@ -4,36 +4,38 @@ import Card from './cardsConstructor.js';
 
 const webCardCreating = () => {
 
-    const cardParentBlock = document.querySelector('.menu__field div.container');
-    
-    new Card(
-        'img/tabs/vegy.jpg', 
-        'vegy', 
-        'Меню "Фитнес"', 
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
-        '', 
-        '229',
-        cardParentBlock
-    ).createNewCard();
-    new Card(
-        'img/tabs/elite.jpg',
-        'elite',
-        'Меню "Премиум"',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        '',
-        '550',
-        cardParentBlock
-    ).createNewCard();
-    new Card(
-        'img/tabs/post.jpg',
-        'post',
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        '',
-        '430',
-        cardParentBlock
-    ).createNewCard();
-    
+    const getData = () => {
+        fetch('http://localhost:3000/menu')
+        .then(data => {
+            if(!data.ok) {
+                throw new Error(`Не удалось получить данные из ${data.url}, статус ${data.status}`);
+            }
+            return data.json();
+        })
+        .then(data => {
+            createCard(data);
+        }).catch(data => {
+            console.error(data);
+        });
+    };
+
+    const createCard = arr => {
+        const cardParentBlock = document.querySelector('.menu__field div.container');
+
+        arr.forEach(data => {
+            new Card(
+                data.img,
+                data.altimg,
+                data.title,
+                data.descr,
+                '',
+                data.price,
+                cardParentBlock
+            ).createNewCard();
+        });
+    };
+
+    getData();
 };
 
 export default webCardCreating;
