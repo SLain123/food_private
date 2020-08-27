327ce028c4a6a94b6302e8e4a3921ece0cb4a914
SLain <sl163@ya.ru>
2020-08-27 12:27:54 +0300

    Добавляет блок индикаторов к слайдеру: Расширение скрипта слайдера, Дополнение стилей слайдера классами для индикаторов.

--- src/js/modules/slider_2.js
+++ src/js/modules/slider_2.js
@@ -6,6 +6,7 @@ const slider = () => {
     const totalSlideSpan = document.querySelector('#total');
     const wrapper = document.querySelector('.offer__slider-wrapper');
     const inner = document.querySelector('.offer__slider-inner');
+    const slider = document.querySelector('.offer__slider');
     let currentSlide = 0;
 
     inner.style.width = `${100 * allSlides.length}${'%'}`; // задает ширину иннер блока по сумме ширины всех слайдов;
@@ -46,7 +47,7 @@ const slider = () => {
             else {
                 currentSlide++;
             }
-        } else {
+        } else if (btn === 'prev'){
             if(currentSlide <= 0) {
                 currentSlide = 3;
             }
@@ -56,6 +57,7 @@ const slider = () => {
         }
         inner.style.transform = `translateX(-${findWidthSlide() * currentSlide}px)`;
         fillCurrentSpan();
+        makeActive();
     };
 
 // Обработчики событий нажатия кнопок и первый запуск функций для счетчика слайдов;
@@ -69,6 +71,57 @@ const slider = () => {
 
     fillTotalSpan();
     fillCurrentSpan();
+
+// ======== Точки Индикаторы ==============
+
+// Функции создают динамически блок индикаторов и наполняют его нужным количеством блоков-ссылок;
+
+    const createIndicateBlock = () => {
+        const div = document.createElement('div');
+        div.classList.add('carousel-indicators');
+
+        for(let i = 0; i < allSlides.length; i++) {
+            createDot(div, i);
+        }
+
+        slider.append(div);
+        makeActive();
+    };
+
+    const createDot = (main, i) => {
+        const div = document.createElement('div');
+        div.classList.add('dot');
+        div.addEventListener('click', () => {
+            switchSlide(i);
+        });
+
+        main.append(div);
+    };
+
+// Функция делает блок-ссылку индикатора активной в соответствии с активным на текущий момент слайдом;
+
+    const makeActive = () => {
+        const dots = document.querySelectorAll('.dot');
+        dots.forEach((dot, i) => {
+            if (currentSlide === i) {
+                dot.classList.add('dot_active');
+            }
+            else {
+                dot.classList.remove('dot_active');
+            }
+        });
+    };
+
+// Функция отвечает за переключение активного слайда при клике на блок-ссылку индикатора;
+
+    const switchSlide = num => {
+        currentSlide = num;
+        nextSlide('');
+    };
+
+// Запуск динамического создания блока индикатора;
+
+    createIndicateBlock();
 };
 
 export default slider;
